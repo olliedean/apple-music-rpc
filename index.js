b4ca7a1858f798565456f07772441191c311d1a4
ollie potterton <ollie@ollies-mbp.home>
2020-12-05 01:21:24 +0000

    Updated to Airbnb's eslint standard

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-rpc = require('discord-rich-presence')('783728735278858253');
-const {execSync} = require('child_process');
-const { parse } = require('path');
+const rpc = require('discord-rich-presence')('783728735278858253');
+const { execSync } = require('child_process');
 const config = require('./config.json');
-var currentSong;
-var currentPosition;
 
-function grabData(){
+let currentSong;
+let currentPosition;
+
+function grabData() {
 	this.artist = execSync("/usr/bin/osascript -e 'tell application \"Music\" to get artist of current track as string'");
 	this.track = execSync("/usr/bin/osascript -e 'tell application \"Music\" to get name of current track as string'");
 	this.position = execSync("/usr/bin/osascript -e 'tell application \"Music\" to get player position'");
@@ -14,47 +14,46 @@ function grabData(){
 	return this;
 }
 
-function updatePresence(paused, details){
+function updatePresence(paused, details) {
 	rpc.updatePresence({
-		state: `${(config.emojis ? "🎧 " : "")}${details.track}`,
-		details: `${(config.emojis ? "🎤 " : "")}${details.artist}`,
+		state: `${(config.emojis ? '🎧 ' : '')}${details.track}`,
+		details: `${(config.emojis ? '🎤 ' : '')}${details.artist}`,
 		endTimestamp: Date.now() + ((parseInt(details.duration) - parseInt(details.position)) * 1000),
 		largeImageKey: 'apple_music_1',
 		largeImageText: 'Apple Music',
-		smallImageKey: `${(paused ? "pause" : "play")}`,
-		smallImageText: `by Zenternal`,
+		smallImageKey: `${(paused ? 'pause' : 'play')}`,
+		smallImageText: 'by Zenternal',
 		instance: true,
 	});
 	currentSong = `${details.artist} - ${details.track}`;
 }
 
-function clearPresence(){
+function clearPresence() {
 	// Still need to figure this out.
 }
 
-function update(){
-	var playerState = execSync("/usr/bin/osascript -e 'tell application \"Music\" to get player state'");
+function update() {
+	let playerState = execSync("/usr/bin/osascript -e 'tell application \"Music\" to get player state'");
 	playerState = `${playerState}`;
 
-	if(playerState.includes("paused")){
-		details = grabData(); 
+	if (playerState.includes('paused')) {
+		const details = grabData();
 		updatePresence(true, details);
-	} else if(playerState.includes("stopped")){
-		if(rpc){  clearPresence() };
-	} else if(playerState.includes("playing")){
-		details = grabData();
-		if(parseInt(details.position) > currentPosition + 5 ||
-			parseInt(details.position) < currentPosition - 5){
+	} else if (playerState.includes('stopped')) {
+		if (rpc) { clearPresence(); }
+	} else if (playerState.includes('playing')) {
+		const details = grabData();
+		if (parseInt(details.position) > currentPosition + 5
+			|| parseInt(details.position) < currentPosition - 5) {
 			return updatePresence(false, details);
 		}
 		currentPosition = parseInt(details.position);
-		if(currentSong === `${details.artist} - ${details.track}`) return;
+		if (currentSong === `${details.artist} - ${details.track}`) return;
 		updatePresence(false, details);
-	} else{
-		if(rpc){ clearPresence(); }
-	}
+	} else if (rpc) { clearPresence(); }
+	return true;
 }
 
 setInterval(() => {
-  update();
-}, 2000)
+	update();
+}, 2000);
